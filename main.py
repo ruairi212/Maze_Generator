9f410141d7d09283539b7b378e222e686898eea4
ruairi <ruairitierney212@gmail.com>
2024-08-16 09:35:39 +0100

    cel changes

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
-#creating a window class for tkinter whi
-class Window:
-    def __init__(self,width: int, height: int):
-        self.__root = Tk()
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__root.title("Maze Generator")
-        self.canvas = Canvas(width=width, height=height)
-        self.run = False
-        self.canvas.pack()
-    
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.run = True
-        while self.run == True:
-            self.redraw()
-
-    def close(self):
-        self.run = False
-class Point:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-class line:
-    def __init__(self,point1,point2):
-        self.point1 = point1
-        self.point2 = point2
-        
-    def draw(self,canvas,fill_color):
-        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y, fill=fill_color, width=2)
-    def draw_line(self,canvas,fill_color):
-        self.draw(canvas,fill_color)
+from gui import Window , Point , line
+from cell import Cell
 def main():
     win = Window(800,600)
-    point1 = Point(200,300)
-    point2 = Point(300,200)
-    myline = line(point1,point2)
-    myline.draw_line(win.canvas,"black")
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell1.draw(10, 60, 10, 60)
+    cell2.draw(70, 120, 10, 60)
     win.wait_for_close()
 main()
