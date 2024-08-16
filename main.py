1cd99d72e45db31ad1189679aeb257e9aba5a1a3
ruairi <ruairitierney212@gmail.com>
2024-08-16 12:07:05 +0100

    "maze"

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from tkinter import Tk, BOTH, Canvas
+
 from gui import Window , Point , line
-from cell import Cell
+from maze import Maze
 def main():
-    win = Window(800,600)
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell1.draw(10, 60, 10, 60)
-    cell2.draw(70, 120, 10, 60)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 main()
